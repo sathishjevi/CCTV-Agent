eabcea8f6fa5ee13d9d0336e926a8d01e5154507
sathishjevi <86569331+sathishjevi@users.noreply.github.com>
2026-09-19 22:34:34 -0400

    firebase project integration changes

--- mobile/floorwatch_app/android/app/build.gradle.kts
+++ mobile/floorwatch_app/android/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // id("com.google.gms.google-services") version "4.5.0" apply false
+    id("com.google.gms.google-services")
 }
 
 // Firebase (push notifications): applied only if android/app/google-services.json
@@ -48,4 +50,23 @@ android {
 
 flutter {
     source = "../.."
+}
+dependencies {
+
+  // Import the Firebase BoM
+
+  implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+
+  // When using the BoM, don't specify versions in Firebase dependencies
+
+  implementation("com.google.firebase:firebase-analytics")
+
+
+  // Add the dependencies for any other desired Firebase products
+
+  // https://firebase.google.com/docs/android/setup#available-libraries
+
 }
